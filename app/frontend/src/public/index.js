a33e4547ffa0433f97f8a4bfa92b49519493426a
dmil <dmil@itu.dk>
2025-02-24 15:48:26 +0100

    add time measuring of 10000 console.log

--- app/frontend/src/public/index.js
+++ app/frontend/src/public/index.js
@@ -2,6 +2,15 @@
 //     console.log(Date.now());
 // }
 
-setInterval(() => {
-    console.log(Date.now());
-}, 1000);
+// setInterval(() => {
+//     console.log(Date.now());
+// }, 1000);
+
+lines = 10000;
+time_start = Date.now();
+for (let i = 0; i < lines; i++) {
+    console.log(`${Date.now()} - ${i}`);
+}
+time_end = Date.now();
+console.log(`Execution Time: ${time_end - time_start} ms`);
+console.log(`Lines/ms: ${lines / (time_end - time_start)}`);
