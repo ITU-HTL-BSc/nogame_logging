e5af015cbeb655f7e51ea902a3a43352cb6d328e
dmil <dmil@itu.dk>
2025-02-26 13:22:08 +0100

    use cors on backend

--- app/backend/src/server.js
+++ app/backend/src/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
 const logger = require("./logger");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/log", (req, res) => {
@@ -14,5 +15,5 @@ app.post("/log", (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
 });
