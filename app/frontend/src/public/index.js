5717a553fba8b78830642942306e410e31f64f5d
Jacob Pærregaard <jacp@itu.dk>
2025-03-10 12:34:51 +0100

    format + change log level to debug

--- app/frontend/src/public/index.js
+++ app/frontend/src/public/index.js
@@ -7,32 +7,32 @@ let countTests = 0;
 
 const testFunction = () => {
     const time_start = performance.now();
-    sendLog("info",time_start);
-    for (let i = 1; i <= lines-1; i++) {
-        sendLog("info",`${performance.now()} - ${i}`);
+    sendLog("debug", time_start);
+    for (let i = 1; i <= lines - 1; i++) {
+        sendLog("debug", `${performance.now()} - ${i}`);
     }
     const time_end = performance.now();
-    sendLog("info",time_end);
+    sendLog("debug", time_end);
 
     countTests++;
     timeTotal += time_end - time_start;
     if (countTests >= maxTests) {
-        sendLog("info",`Execution Time: ${timeTotal} ms`);
-        sendLog("info",`Average Time: ${timeTotal / maxTests} ms`);
-        sendLog("info",`Lines/ms: ${lines / (timeTotal / maxTests)}`);
+        sendLog("info", `Execution Time: ${timeTotal} ms`);
+        sendLog("info", `Average Time: ${timeTotal / maxTests} ms`);
+        sendLog("info", `Lines/ms: ${lines / (timeTotal / maxTests)}`);
         clearInterval(interval);
     }
-}
+};
 
 const sendLog = async (level, msg) => {
     const response = await fetch("http://localhost:3000/log", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify({ level, id, msg })
+        body: JSON.stringify({ level, id, msg }),
     });
     return response;
-}
+};
 
 const interval = setInterval(testFunction, 2000);
