af7606046c1e3edf77bcc3e2419796a8ac1d69a1
dmil <dmil@itu.dk>
2025-03-10 12:40:55 +0100

    Fix 1 too many log lines

--- app/frontend/src/public/index.js
+++ app/frontend/src/public/index.js
@@ -7,19 +7,19 @@ let countTests = 0;
 
 const testFunction = () => {
     const time_start = performance.now();
-    sendLog("debug", time_start);
-    for (let i = 1; i <= lines - 1; i++) {
-        sendLog("debug", `${performance.now()} - ${i}`);
+    sendLog("info", time_start);
+    for (let i = 1; i < lines - 1; i++) {
+        sendLog("info", `${performance.now()} - ${i}`);
     }
     const time_end = performance.now();
-    sendLog("debug", time_end);
+    sendLog("info", time_end);
 
     countTests++;
     timeTotal += time_end - time_start;
     if (countTests >= maxTests) {
-        sendLog("info", `Execution Time: ${timeTotal} ms`);
-        sendLog("info", `Average Time: ${timeTotal / maxTests} ms`);
-        sendLog("info", `Lines/ms: ${lines / (timeTotal / maxTests)}`);
+        sendLog("metric", `Execution Time: ${timeTotal} ms`);
+        sendLog("metric", `Average Time: ${timeTotal / maxTests} ms`);
+        sendLog("metric", `Lines/ms: ${lines / (timeTotal / maxTests)}`);
         clearInterval(interval);
     }
 };
