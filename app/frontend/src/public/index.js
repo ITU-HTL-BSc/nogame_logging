b6d79038241fcf31ff1202f6110eae5b246205d0
dmil <dmil@itu.dk>
2025-03-13 17:55:57 +0100

    single test 10k lines

--- app/frontend/src/public/index.js
+++ app/frontend/src/public/index.js
@@ -3,36 +3,28 @@ const id = crypto.randomUUID();
 const logger = JL(`${id}`);
 
 var ajaxAppender = JL.createAjaxAppender(`${id}`);
-ajaxAppender.setOptions({ 
-    "url": "http://localhost:3000/log", 
+ajaxAppender.setOptions({
+    "url": "http://localhost:3000/log",
     "batchSize": 1,
     "maxBatchSize": 1,
+    "batchTimeout": 0,
 });
 
 JL().setOptions({ "appenders": [ajaxAppender] });
 
-const lines = 1000;
-const maxTests = 10;
-let timeTotal = 0;
-let countTests = 0;
+const lines = 10000;
 
 const testFunction = () => {
     const time_start = performance.now();
-    logger.info(time_start);
-    for (let i = 1; i < lines - 1; i++) {
+    for (let i = 0; i < lines; i++) {
         logger.info(`${performance.now()} - ${i}`);
     }
     const time_end = performance.now();
-    logger.info(time_end);
 
-    countTests++;
-    timeTotal += time_end - time_start;
-    if (countTests >= maxTests) {
-        logger.log(3500, `Execution Time: ${timeTotal} ms`);
-        logger.log(3500, `Average Time: ${timeTotal / maxTests} ms`);
-        logger.log(3500, `Lines/ms: ${lines / (timeTotal / maxTests)}`);
-        clearInterval(interval);
-    }
+    const timeTotal = time_end - time_start;
+
+    logger.log(3500, `Execution Time: ${timeTotal} ms`);
+    logger.log(3500, `Lines/ms: ${lines / timeTotal}`);
 }
 
-const interval = setInterval(testFunction, 2000);
+testFunction();
