c100b47a35d1e1e0d62b939d980425e29bb0eafd
dmil <dmil@itu.dk>
2025-03-13 14:50:47 +0100

    run synchronous, 10k line runs

--- app/frontend/src/public/index.js
+++ app/frontend/src/public/index.js
@@ -1,27 +1,20 @@
 const id = crypto.randomUUID();
 
-const lines = 1000;
-const maxTests = 10;
-let timeTotal = 0;
-let countTests = 0;
+const lines = 10000;
 
-const testFunction = () => {
+const testFunction = async () => {
     const time_start = performance.now();
-    sendLog("info",time_start);    
-    for (let i = 1; i< lines-1; i++) {
-        sendLog("info",`${performance.now()} - ${i}`);
+    await sendLog("info", time_start);
+    for (let i = 1; i < lines - 1; i++) {
+        await sendLog("info", `${performance.now()} - ${i}`);
     }
     const time_end = performance.now();
-    sendLog("info",time_end);
+    await sendLog("info", time_end);
 
-    countTests++;
-    timeTotal += time_end - time_start;
-    if (countTests >= maxTests) {
-        sendLog("info", `Execution Time: ${timeTotal} ms`);
-        sendLog("info", `Average Time: ${timeTotal / maxTests} ms`);
-        sendLog("info", `Lines/ms: ${lines / (timeTotal / maxTests)}`);
-        clearInterval(interval);
-    }
+    const timeTotal = time_end - time_start;
+
+    sendLog("info", `Execution Time: ${timeTotal} ms`);
+    sendLog("info", `Lines/ms: ${lines / timeTotal}`);
 }
 
 const sendLog = async (level, msg) => {
@@ -41,4 +34,4 @@ const sendLog = async (level, msg) => {
     }
 }
 
-const interval = setInterval(testFunction, 2000);
+testFunction();
