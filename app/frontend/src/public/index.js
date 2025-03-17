2b65e9cc0c709041bd7101d7ab227e26b895575b
Jacob Pærregaard <jacp@itu.dk>
2025-03-17 11:39:29 +0100

    FIX MAIN

--- app/frontend/src/public/index.js
+++ app/frontend/src/public/index.js
@@ -4,12 +4,12 @@ const lines = 10000;
 
 const testFunction = async () => {
     const time_start = performance.now();
-    await sendLog("info", time_start);
+    console.log(time_start);
     for (let i = 1; i < lines - 1; i++) {
-        await sendLog("info", `${performance.now()} - ${i}`);
+        console.log(`${performance.now()} - ${i}`);
     }
     const time_end = performance.now();
-    await sendLog("info", time_end);
+    console.log(time_end);
 
     const timeTotal = time_end - time_start;
 
@@ -22,17 +22,6 @@ const testFunction = async () => {
             mode: "cors",
         }
     );
-};
-
-const sendLog = async (level, msg) => {
-    const response = await fetch("http://localhost:3000/log", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ level, id, msg }),
-    });
-    return response;
 };
 
 testFunction();
