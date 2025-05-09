9699870f17df44e6fb5ab0d441adc018da0f799c
Jacob Pærregaard <jacp@itu.dk>
2025-05-09 15:25:34 +0200

    broom did stuff.

--- app/frontend/src/public/index.js
+++ app/frontend/src/public/index.js
@@ -1,32 +1,30 @@
-const id = crypto.randomUUID();
-
 const lines = 10000;
 
 const testFunction = async () => {
     const time_start = performance.now();
     await sendLog("info", time_start);
     for (let i = 1; i < lines - 1; i++) {
-        await sendLog("info", `${performance.now()} - ${i}`);
+        await sendLog("info", `${i}`);
     }
     const time_end = performance.now();
     await sendLog("info", time_end);
 
-    const timeTotal = time_end - time_start;
+    const time_total = time_end - time_start;
 
-    await axios.post(
-        `http://localhost:3000/metric?exec_time=${timeTotal}&lines_per_sec=${
-            lines / timeTotal
-        }`
-    );
+    await axios.post(`http://localhost:3000/metric?exec_time=${time_total}`);
 };
 
 const sendLog = async (level, msg) => {
     const response = await axios.post("http://localhost:3000/log", {
         level,
-        id,
         msg,
     });
     return response;
 };
 
-testFunction();
+(async () => {
+    for (let i = 0; i < 20; i++) {
+        await testFunction();
+        document.getElementById("counter").innerHTML = ` ${i + 1}`;
+    }
+})();
